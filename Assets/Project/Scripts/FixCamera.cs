061e6692162bbf5cd2a6a47eec6acb00ae007a38
Argensik <d.legizynski@gmail.com>
2019-09-13 14:53:28 +0200

    Initial commit

---
+++ Assets/Project/Scripts/FixCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixCamera : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = new Vector2(
+                target.transform.position.x,
+                0f);
+        }
+    }
+}
