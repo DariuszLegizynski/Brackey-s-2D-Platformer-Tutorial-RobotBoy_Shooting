fdddf09ee90dee195e8a2fb3bd0a9dd0bb09449f
Argensik <d.legizynski@gmail.com>
2019-09-20 14:52:05 +0200

    Adding a muzle

---
+++ Assets/Project/Scripts/WeaponController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public float fireRate = 0f;
+    public float damage = 10f;
+    public LayerMask notToHit;
+
+    bool reloadNeeded;
+
+    private float timeToFire = 0;
+    Transform muzle;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        muzle = transform.FindChild("Muzle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
