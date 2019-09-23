b6d8ccdde8229717816ec7dabf397fe75dc398f8
Argensik <d.legizynski@gmail.com>
2019-09-23 06:30:10 +0200

    Working on shooting logic

--- Assets/Project/Scripts/WeaponController.cs
+++ Assets/Project/Scripts/WeaponController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class WeaponController : MonoBehaviour
 {
     public float fireRate = 0f;
+    //for future use
+    //public int ammoRate = 0;
     public float damage = 10f;
     public LayerMask notToHit;
 
-    bool reloadNeeded;
+    bool ifReloaded = true;
 
     private float timeToFire = 0;
     Transform muzle;
@@ -16,12 +18,59 @@ public class WeaponController : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        muzle = transform.FindChild("Muzle");
+        muzle = transform.Find("Muzle");
+        
+        if(muzle == null)
+        {
+            Debug.LogError("No muzle object found");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GunFire();
+    }
+
+    void GunFire()
+    {
+        if (fireRate == 0)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                //Play sound "Gun Aim"
+
+                if (Input.GetKeyUp(KeyCode.D) && ifReloaded == true)
+                {
+                    Shoot();
+                }
+
+                else if((Input.GetKeyUp(KeyCode.D) && ifReloaded == true))
+                {
+                    //PlaySound.EmptyMagazine("Click");
+                    Debug.Log("Reload!");
+                }
+
+                else 
+                {
+                    Debug.LogError("Something wrong with the shooting logic!");
+                }
+            }
+
+            else
+            {
+                //Play sound. no more aiming
+                Debug.Log("Not aiming anymore");
+            }
+        }
+
+        else
+        {
+            if (Input.GetKey(KeyCode.D) && Time.time > timeToFire);
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
     }
 }
