d13d074c68f27d09779579678d50472b890c83c4
Argensik <d.legizynski@gmail.com>
2019-09-25 06:30:42 +0200

    Trying to shoot in the x direction of the muzle

--- Assets/Project/Scripts/WeaponController.cs
+++ Assets/Project/Scripts/WeaponController.cs
@@ -81,10 +81,11 @@ public class WeaponController : MonoBehaviour
 
     void Shoot()
     {
+        //Vector2 muzle = new Vector2(transform.position.x, transform.position.y);
         Vector2 muzle = transform.position;
-            //new Vector2(muzle.position.x, muzle.position.y);
-        RaycastHit2D hit = Physics2D.Raycast(muzle, Vector2.right, 100f, notToHit);
-        Debug.DrawLine(muzle, Vector2.right * 100f);
+        //Vector2 muzle = new Vector2(muzle.position.x, muzle.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(muzle, Vector2.right * 100f, 100f, notToHit);
+        Debug.DrawRay(muzle, Vector2.right * 100f);
         Debug.LogError("PifPaf!");
     }
 
